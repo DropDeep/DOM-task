5af86e46812d55f011a01bff18aedbe111350811
Max <dudarikm@gmail.com>
2022-02-17 19:04:39 +0200

    one with refactoring

--- js/script.js
+++ js/script.js
@@ -1,61 +1,94 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener("DOMContentLoaded", () => {
 
-// 1)
-const ads = document.querySelectorAll('.promo__adv img'),
-      promoBg = document.querySelector('.promo__bg'),
-      genre = promoBg.querySelector('.promo__genre'),
-      filmContent = document.querySelector(".promo__interactive-list");
-    //   listOfFilms = filmContent.getElementsByTagName('li');
-  
- ads.forEach(img => {
-    img.remove();
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    // 1)
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector(".promo__interactive-list"),
+        addForm = document.querySelector("form.add"),
+        addInput = addForm.querySelector(".adding__input"),
+        checkbox = addForm.querySelector("[type='checkbox']");
+
+
+    addForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favourite = checkbox.checked;
+
+
+        if (newFilm) {
+
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if(favourite) {
+                console.log("We add new favourite film to the list");
+            }
+            
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+
+    });
+
+    const deleteAdv = function (arr) {
+        arr.forEach(img => {
+            img.remove();
+        });
+    };
+
+    const makeChanges = function () {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url("/img/bg.jpg")';
+    };
+
+    const sortArr = function (arr) {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';
+        sortArr(films);
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} - ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll(".delete").forEach((btn, i)=> {
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 });
-
-genre.textContent = 'драма';
-
-promoBg.style.backgroundImage = 'url("/img/bg.jpg")';
-
-filmContent.innerHTML = '';
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    filmContent.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} - ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
-// for (let i = 0; i < listOfFilms.length; i++) {
-//     listOfFilms[i].textContent = `${i + 1} - ${movieDB.movies[i]}`; 
-// }
-
-// 5)
-// done in 4th
-
-
