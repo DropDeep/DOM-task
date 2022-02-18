ea22dbe7206c4287f746da35e470f4dff9a3a972
DropDeep <dudarikm@gmail.com>
2022-02-18 18:08:17 +0200

    testing new machine

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -90,5 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
     deleteAdv(adv);
     makeChanges();
     createMovieList(movieDB.movies, movieList);
+
+    console.log("test");
 
 });
